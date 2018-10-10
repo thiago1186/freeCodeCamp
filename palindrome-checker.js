61740262fd8f7a2928abcd51e0e215294e1aea14
Thiago de Souza do Nascimento <thiago.souza.ctis@ibge.gov.br>
2018-10-10 09:06:24 -0300

    Retirada a chamada para a função

--- palindrome-checker.js
+++ palindrome-checker.js
@@ -2,7 +2,3 @@ function palindrome(str) {
   let cleanStr = str.toLowerCase().replace(/[^0-9a-z]/g,"");
   return cleanStr.split("").reverse().join("") == cleanStr;
 }
-
-
-
-palindrome("A man, a plan, a canal. Panama");
